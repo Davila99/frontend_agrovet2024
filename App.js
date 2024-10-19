f558e66dc3decf72fec6cffd81fb330cc35ad624
Arlen Assael Marenco Jaime <91499118+NightCW34@users.noreply.github.com>
2024-10-19 03:40:53 -0600

    Last Update

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
+  DrawerItem,
 } from "@react-navigation/drawer";
 
 import {
@@ -23,205 +24,230 @@ import {
   Works,
 } from "./Screens/Index";
 
-//Bottom Tab Imports
+// Bottom Tab Imports
 import Agronomos from "./Screens/BottomMainScreens/Agronomos";
 import Veterinarios from "./Screens/BottomMainScreens/Veterinarios";
 import Anuncios from "./Screens/BottomMainScreens/Anuncios";
 import Chats from "./Screens/BottomMainScreens/Chats";
 import HomeScreen from "./Screens/BottomMainScreens/Home";
 import IMAGES from "./Constants/Img";
+import UserManual from "./Screens/BottomMainScreens/UserManual";
+import UserProfile from "./Screens/BottomMainScreens/UserProfile";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-export default function App() {
-  const HomeScreenWithDrawer = () => {
-    return (
-      <Drawer.Navigator
-        initialRouteName="HomeScreen"
-        drawerContent={() => (
-          <DrawerContentScrollView>
-            <Text>Drawer Content</Text>
-          </DrawerContentScrollView>
-        )}
-      >
-        <Drawer.Screen name="HomeScreen" component={HomeScreen} />
-      </Drawer.Navigator>
-    );
-  };
+const DrawerContent = (props) => (
+  <DrawerContentScrollView {...props}>
+    <Text style={{ textAlign: "center" }}>Menú de Contenido</Text>
+    <DrawerItem
+      label={"Perfil de Usuario"}
+      onPress={() => props.navigation.navigate("UserProfile")}
+    />
+    <DrawerItem
+      label={"Manual de Usuario"}
+      onPress={() => props.navigation.navigate("UserManual")}
+    />
+  </DrawerContentScrollView>
+);
 
-  const TabNavigator = () => {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Agronomos"
-          component={Agronomos}
-          options={{
-            headerShown: false,
-            title: "Agrónomos",
-            tabBarLabelStyle: {
-              fontSize: 14,
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={IMAGES.AGRONOMOS}
-                style={{
-                  height: 30,
-                  width: 30,
-                  tintColor: focused ? COLORS.blue : COLORS.lightPrimary,
-                }}
-              />
-            ),
-          }}
-        />
-
-        <Tab.Screen
-          name="Veterinarios"
-          component={Veterinarios}
-          options={{
-            title: "Veterinarios",
-            tabBarLabelStyle: {
-              fontSize: 14,
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={IMAGES.VETERINARIO}
-                style={{
-                  height: 30,
-                  width: 30,
-                  tintColor: focused ? COLORS.blue : COLORS.lightPrimary,
-                }}
-              />
-            ),
-            tabBarActiveTintColor: COLORS.blue,
-            tabBarInactiveTintColor: COLORS.black,
-          }}
-        />
-        <Tab.Screen
-          name="Home"
-          component={HomeScreenWithDrawer}
-          options={{
-            headerShown: false,
-            title: "Home",
-            tabBarLabelStyle: {
-              fontSize: 14,
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={IMAGES.HOME}
-                style={{
-                  height: 30,
-                  width: 30,
-                  tintColor: focused ? COLORS.blue : COLORS.lightPrimary,
-                }}
-              />
-            ),
-            tabBarActiveTintColor: COLORS.blue,
-            tabBarInactiveTintColor: COLORS.black,
-          }}
-        />
-        <Tab.Screen
-          name="Anuncios"
-          component={Anuncios}
-          options={{
-            headerShown: false,
-            title: "Anuncios",
-            tabBarLabelStyle: {
-              fontSize: 14,
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={IMAGES.ANUNCIO}
-                style={{
-                  height: 30,
-                  width: 30,
-                  tintColor: focused ? COLORS.blue : COLORS.lightPrimary,
-                }}
-              />
-            ),
-            tabBarActiveTintColor: COLORS.blue,
-            tabBarInactiveTintColor: COLORS.black,
-          }}
-        />
-        <Tab.Screen
-          name="Chats"
-          component={Chats}
-          options={{
-            title: "Chats",
-            tabBarLabelStyle: {
-              fontSize: 14,
-            },
-            tabBarIcon: ({ focused }) => (
-              <Image
-                source={IMAGES.CHAT}
-                style={{
-                  height: 30,
-                  width: 30,
-                  tintColor: focused ? COLORS.blue : COLORS.lightPrimary,
-                  headerShown: false,
-                }}
-              />
-            ),
-            tabBarActiveTintColor: COLORS.blue,
-            tabBarInactiveTintColor: COLORS.black,
-          }}
-        />
-      </Tab.Navigator>
-    );
-  };
-
+const HomeWithDrawer = () => {
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Welcome">
-          <Stack.Screen
-            name="Welcome"
-            component={Welcome}
-            options={{
-              headerShown: false,
-            }}
-          />
+    <Drawer.Navigator
+      drawerContent={(props) => <DrawerContent {...props} />}
+      screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="HomeTabs" component={TabNavigator} />
+      <Drawer.Screen name="UserProfile" component={UserProfile} />
+      <Drawer.Screen name="UserManual" component={UserManual} />
+    </Drawer.Navigator>
+  );
+};
 
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              headerShown: false,
-            }}
-          />
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          height: 70, // Ajusta la altura a tu preferencia
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Agronomos"
+        component={Agronomos}
+        options={{
+          headerShown: true,
+          title: "Agrónomos",
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={IMAGES.AGRONOMOS}
+              style={{
+                height: 25,
+                width: 25,
+                tintColor: focused ? COLORS.blue : COLORS.black,
+              }}
+            />
+          ),
+          tabBarActiveTintColor: COLORS.blue,
+          tabBarInactiveTintColor: COLORS.black,
+        }}
+      />
+      <Tab.Screen
+        name="Veterinarios"
+        component={Veterinarios}
+        options={{
+          headerShown: true,
+          title: "Veterinarios",
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={IMAGES.VETERINARIO}
+              style={{
+                height: 25,
+                width: 25,
+                tintColor: focused ? COLORS.blue : COLORS.black,
+              }}
+            />
+          ),
+          tabBarActiveTintColor: COLORS.blue,
+          tabBarInactiveTintColor: COLORS.black,
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: true,
+          title: "Home",
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={IMAGES.HOME}
+              style={{
+                height: 25,
+                width: 25,
+                tintColor: focused ? COLORS.blue : COLORS.black,
+              }}
+            />
+          ),
+          tabBarActiveTintColor: COLORS.blue,
+          tabBarInactiveTintColor: COLORS.black,
+        }}
+      />
+      <Tab.Screen
+        name="Anuncios"
+        component={Anuncios}
+        options={{
+          headerShown: true,
+          title: "Anuncios",
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={IMAGES.ANUNCIO}
+              style={{
+                height: 25,
+                width: 25,
+                tintColor: focused ? COLORS.blue : COLORS.black,
+              }}
+            />
+          ),
+          tabBarActiveTintColor: COLORS.blue,
+          tabBarInactiveTintColor: COLORS.black,
+        }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={Chats}
+        options={{
+          headerShown: true,
+          title: "Chats",
+          tabBarLabelStyle: {
+            fontSize: 14,
+          },
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={IMAGES.CHAT}
+              style={{
+                height: 25,
+                width: 25,
+                tintColor: focused ? COLORS.blue : COLORS.black,
+              }}
+            />
+          ),
+          tabBarActiveTintColor: COLORS.blue,
+          tabBarInactiveTintColor: COLORS.black,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-          <Stack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="PerfilUser"
-            component={PerfilUser}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="VetList"
-            component={TabNavigator}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Works"
-            component={Works}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="HomeWithDrawer"
+          component={HomeWithDrawer}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="PerfilUser"
+          component={PerfilUser}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="VetList"
+          component={TabNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Works"
+          component={Works}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screens/BottomMainScreens/UserManual.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function UserManual() {
+  return (
+    <View>
+      <Text>UserManual</Text>
+    </View>
+  );
+}

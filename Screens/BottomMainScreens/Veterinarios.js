5f4a55432510864d926fe8328ce98aaf9aa734cf
Arlen Assael Marenco Jaime <91499118+NightCW34@users.noreply.github.com>
2024-10-18 20:02:46 -0600

    Test Upload 2

---
+++ Screens/BottomMainScreens/Veterinarios.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Veterinarios = () => {
+  return (
+    <View>
+      <Text>Veterinarios</Text>
+    </View>
+  );
+};
+
+export default Veterinarios;
